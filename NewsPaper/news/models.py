15d30370c5dd49b910d7a4655dffe023cc8c8e80
nevadimka <myregbox@outlook.com>
2022-02-02 16:03:07 +0300

    Models filled.

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -1,3 +1,60 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating = models.SmallIntegerField(default=0)
+
+    def update_rating(self):
+        pass
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+
+
+class Post(models.Model):
+    NEWS = 'NW'
+    ARTICLE = 'AR'
+    CATEGORY_CHOICES = (
+        (NEWS, 'Новость'),
+        (ARTICLE, 'Статья'),
+    )
+
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category_type = models.CharField(
+        max_length=2, choices=CATEGORY_CHOICES, default=ARTICLE)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    categories = models.ManyToManyField(Category, through='PostCategory')
+    title = models.CharField(max_length=128)
+    text = models.TextField()
+    rating = models.SmallIntegerField(default=0)
+
+    def preview(self):
+        pass
+
+    def like(self):
+        pass
+
+    def dislike(self):
+        pass
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    rating = models.SmallIntegerField(default=0)
+
+    def like(self):
+        pass
+
+    def dislike(self):
+        pass
