3a74f8a34fa913d2e78afb9ff26d98e0ced98a9a
nevadimka <myregbox@outlook.com>
2022-03-13 17:58:01 +0300

    Unit D5.6: downgrage action fixed.

--- NewsPaper/sign/urls.py
+++ NewsPaper/sign/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView
-from .views import upgrade_me
+from .views import upgrade_me, downgrade_me
 
 urlpatterns = [
     path('logout/',
          LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('upgrade/', upgrade_me, name='upgrade'),
-    path('downgrade/', upgrade_me, name='downgrade'),
+    path('downgrade/', downgrade_me, name='downgrade'),
 ]
