f89df7dde807eb0a5e6e3050b83ab659101439a5
nevadimka <myregbox@outlook.com>
2022-03-16 21:28:46 +0300

    Unit D7.5: refactoring - mail actions moved to mail_manager.py.

---
+++ NewsPaper/news/mail_manager.py
@@ -0,0 +1,63 @@
+from django.core import mail
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+
+from .models import Category
+
+
+def send_email_by_signal(new_post):
+    cat = Category.objects.get(pk=1)    # пока добавляем просто первую категорию
+    new_post.categories.add(cat)
+    mail_messages = []
+    for subscriber in cat.subscribers.all():
+        mail_messages.append(create_one_time_mail_message(new_post, subscriber))
+    send_multiple_emails(mail_messages)
+
+
+def send_email_by_scheduler(subscribers_posts):
+    mail_messages = []
+    for subscriber in subscribers_posts.keys():
+        mail_messages.append(create_weekly_mail_message(subscriber, subscribers_posts[subscriber]))
+    send_multiple_emails(mail_messages)
+
+
+def send_multiple_emails(mail_messages):
+    if len(mail_messages) > 0:
+        connection = mail.get_connection()
+        connection.send_messages(mail_messages)
+
+
+def create_one_time_mail_message(new_post, subscriber):
+    html_content = render_to_string(
+        'mail_new_post_notification.html',
+        {
+            'post': new_post,
+            'user': subscriber,
+        }
+    )
+    msg = EmailMultiAlternatives(
+        subject=f'New post: {new_post.title}',
+        body=new_post.text,
+        from_email='skillfactorymailserver@yandex.ru',
+        to=[subscriber.email],
+    )
+    msg.attach_alternative(html_content, "text/html")
+    return msg
+
+
+def create_weekly_mail_message(subscriber, weekly_posts):
+    html_content = render_to_string(
+        'mail_weekly_notification.html',
+        {
+            'weekly_posts': weekly_posts,
+            'user': subscriber,
+        }
+    )
+    msg = EmailMultiAlternatives(
+        subject='Your weekly collection of publications',
+        body='some text in body',
+        from_email='skillfactorymailserver@yandex.ru',
+        to=[subscriber.email],
+    )
+    msg.attach_alternative(html_content, "text/html")
+    return msg
--- NewsPaper/news/management/commands/runapscheduler.py
+++ NewsPaper/news/management/commands/runapscheduler.py
@@ -6,13 +6,11 @@ from django.conf import settings
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
-from django.core import mail
-from django.core.mail import EmailMultiAlternatives
 from django.core.management.base import BaseCommand
-from django.template.loader import render_to_string
 from django_apscheduler.jobstores import DjangoJobStore
 from django_apscheduler.models import DjangoJobExecution
 
+from ...mail_manager import send_email_by_scheduler
 from ...models import Post
 
 logger = logging.getLogger(__name__)
@@ -20,50 +18,21 @@ logger = logging.getLogger(__name__)
 
 # наша задача по выводу текста на экран
 def weekly_mail_notification():
-    dic = defaultdict(list)
+    subscribers_posts = defaultdict(list)
     today = datetime.now()
     week_ago = today - timedelta(days=7)
     for post in Post.objects.filter(creation_date__range=[week_ago, today]):
         for category in post.categories.all():
             for subscriber in category.subscribers.all():
-                dic[subscriber].append(post)
-    # формирование рассылки
-    mail_messages = []
-    for subscriber in dic.keys():
-        mail_messages.append(create_mail_message(subscriber, dic[subscriber]))
-
-    if len(mail_messages) > 0:
-        connection = mail.get_connection()
-        # Manually open the connection
-        connection.open()
-        connection.send_messages(mail_messages)
-        # We need to manually close the connection.
-        connection.close()
-
+                subscribers_posts[subscriber].append(post)
+    send_email_by_scheduler(subscribers_posts)
+    print("letters have been sent...")
     # debug print
-    # print(dic.items())
-    # for user in dic.keys():
+    # print(subscribers_posts.items())
+    # for user in subscribers_posts.keys():
     #     print(user.username)
-    #     for post in dic[user]:
+    #     for post in subscribers_posts[user]:
     #         print(post.title)
-
-
-def create_mail_message(subscriber, weekly_posts):
-    html_content = render_to_string(
-        'mail_weekly_notification.html',
-        {
-            'weekly_posts': weekly_posts,
-            'user': subscriber,
-        }
-    )
-    msg = EmailMultiAlternatives(
-        subject='Your weekly collection of publications',
-        body='some text in body',
-        from_email='skillfactorymailserver@yandex.ru',
-        to=[subscriber.email],
-    )
-    msg.attach_alternative(html_content, "text/html")
-    return msg
 
 
 # функция, которая будет удалять неактуальные задачи
@@ -82,8 +51,8 @@ class Command(BaseCommand):
         # добавляем работу нашему задачнику
         scheduler.add_job(
             weekly_mail_notification,
-            # trigger=CronTrigger(second="*/10"),
-            trigger=CronTrigger(day_of_week="thu"),
+            trigger=CronTrigger(second="*/10"),
+            # trigger=CronTrigger(day_of_week="thu"),
             # То же, что и интервал, но задача тригера таким образом более понятна django
             id="weekly_mail_notification",  # уникальный айди
             max_instances=1,
