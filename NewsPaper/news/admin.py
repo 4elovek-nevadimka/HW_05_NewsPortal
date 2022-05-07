80531a9fd8051d8c83e9d7d08044671457ddab91
nevadimka <myregbox@outlook.com>
2022-05-07 17:06:23 +0300

    Unit D14.4: model translation

--- NewsPaper/news/admin.py
+++ NewsPaper/news/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
+from modeltranslation.admin import TranslationAdmin
+
 from .models import Post
+
+
+class PostAdmin(TranslationAdmin):
+    model = Post
+
 
 admin.site.register(Post)
