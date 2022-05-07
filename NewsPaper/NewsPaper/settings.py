ba2d0e551ed09a5b3d8213b709656dba97855c57
nevadimka <myregbox@outlook.com>
2022-05-07 18:07:19 +0300

    Unit D14.5: timezone added

--- NewsPaper/NewsPaper/settings.py
+++ NewsPaper/NewsPaper/settings.py
@@ -64,6 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'news.middleware.TimezoneMiddleware',
 ]
 
 ROOT_URLCONF = 'NewsPaper.urls'
--- NewsPaper/NewsPaper/views.py
+++ NewsPaper/NewsPaper/views.py
@@ -1,17 +1,23 @@
+import pytz
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils.translation import gettext as _
+from django.utils import timezone
 
-
-# Create your views here.
 from django.views import View
 
 
 class Index(View):
     def get(self, request):
         hello_world = _('Hello world')
-        
+
         context = {
-            'hello_world': hello_world
+            'hello_world': hello_world,
+            'current_time': timezone.now(),
+            'timezones': pytz.common_timezones
         }
         return HttpResponse(render(request, 'index.html', context))
+
+    def post(self, request):
+        request.session['django_timezone'] = request.POST['timezone']
+        return redirect('/')
